35553869c21585b059f4a9fc7f34984a6cf95968
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2024-06-09 09:32:17 -0300

    Modulo 6 Curso Codans Jamilton Desenvolvedor Dev kotlin-Java--- projeto completo - commitado e o essencial é estuda-lo ponto a ponto até entender cada racicionio dentro do projeto

--- app/src/main/java/com/example/aulafrasesdodia/MainActivity.java
+++ app/src/main/java/com/example/aulafrasesdodia/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aulafrasesdodia;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    // depois de criar metodos preciso criar os atributos
+
+    String [] frases = {
+
+            "Aprendi uma nova forma de usar o for quando possuo array, colocando string frase:frases",
+            "Entendi também que os atributos são criados na classe principal MainActivity para serem chamados nos metodos public void verbo(View view){}",
+            "preciso entender porque eu tenho nesse projeto o metodo onCreate e porque somente coloquei fora dele os dois metodos dos botões",
+            "Realmente não posso ir correndo como maratona de serie e sim ir compreendendo até absrover cada evento; hoje vou imprimir esse projeto",
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,26 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+public void gerarFrase(View view){
 
+    TextView texto = findViewById(R.id.textResultado);
+    int numeroAletaorio = new Random().nextInt(4);
+    String frase = frases [numeroAletaorio];
+    texto.setText(frase);
+}
+
+    public void exibirTodas(View view){
+
+        TextView texto = findViewById(R.id.textResultado);
+
+
+
+        String textoResultado ="";
+
+        for (String frase: frases){
+
+            textoResultado = textoResultado + frase+ "\n";
+        }
+        texto.setText( textoResultado );
+    }
 }
